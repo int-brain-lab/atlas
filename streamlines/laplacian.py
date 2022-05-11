e5727382b6165d229741fe193c4b82d8d4b89ac1
Cyrille Rossant <rossant@users.noreply.github.com>
2022-05-11 15:38:19 +0200

    WIP: von Neumann boundary conditions

--- streamlines/laplacian.py
+++ streamlines/laplacian.py
@@ -26,7 +26,7 @@ ROOT_PATH = Path(__file__).parent.resolve()
 CCF_PATH = Path("../ccf_2017/").resolve()
 
 REGION = 'isocortex'
-ITERATIONS = 100
+ITERATIONS = 900
 REGION_ID = 315
 N, M, P = 1320, 800, 1140
 
@@ -233,11 +233,31 @@ def laplace(Uin, Uout, M, nc, mc, pc):
                 Uin[i, j, k + 1])
 
 
+@jit.rawkernel()
+def vonneumann(Uout, M, Ni, Nj, Nk, nc, mc, pc):
+    # The 3 arrays M, Uin, Uout have size (nc, mc, pc)
+
+    # Current voxel
+    i = 1 + jit.blockIdx.x * jit.blockDim.x + jit.threadIdx.x
+    j = 1 + jit.blockIdx.y * jit.blockDim.y + jit.threadIdx.y
+    k = 1 + jit.blockIdx.z * jit.blockDim.z + jit.threadIdx.z
+
+    if (1 <= i) and (1 <= j) and (1 <= k) and (i <= nc - 2) and (j <= mc - 2) and (k <= pc - 2):
+        m = M[i, j, k]
+        if m  == V_S1 or m == V_S2 or m == V_Si:
+            ni = Ni[i, j, k]
+            nj = Nj[i, j, k]
+            nk = Nk[i, j, k]
+            Uout[i, j, k] = (Uout[i+ni,j,k] + Uout[i,j+nj,k] + Uout[i,j,k+nk] - 1) / 3.0
+
+
 class Runner:
-    def __init__(self, M, U=None):
+    def __init__(self, M, normal, U=None):
         n, m, p = M.shape
         self.shape = (n, m, p)
         assert M.dtype == np.uint8
+        assert normal.dtype == np.int8
+        assert normal.shape == self.shape + (3,)
 
         # Compute the bounding box of the mask.
         print("Computing the bounding box of the mask volume...")
@@ -246,11 +266,19 @@ class Runner:
         assert mc > 0
         assert pc > 0
 
-        # Create the M matrix encoding S1, S2, and the mask.
+        # Mask.
         size = nc * mc * pc / 1024. ** 2
-        print(f"Creating one array of total size {size:.2f} MB on the GPU...")
+        print(f"Creating mask array of total size {size:.2f} MB on the GPU...")
         # Transfer M from CPU to GPU.
         Mgpu = cp.asarray(M[box])
+
+        # Normal.
+        size = 3 * nc * mc * pc / 1024. ** 2
+        print(f"Creating 3 normal arrays of total size {size:.2f} MB on the GPU...")
+        # Transfer M from CPU to GPU.
+        Ni_gpu = cp.asarray(normal[..., 0][box])
+        Nj_gpu = cp.asarray(normal[..., 1][box])
+        Nk_gpu = cp.asarray(normal[..., 2][box])
 
         # Create the two scalar fields.
         size = 2 * nc * mc * pc * 4 / 1024. ** 2
@@ -276,6 +304,9 @@ class Runner:
         self.args = (cp.int32(nc), cp.int32(mc), cp.int32(pc))
 
         self.M = Mgpu
+        self.Ni = Ni_gpu
+        self.Nj = Nj_gpu
+        self.Nk = Nk_gpu
         self.Ua = Ua
         self.Ub = Ub
         self.box = box
@@ -285,7 +316,10 @@ class Runner:
         # the Laplacian in parallel on the GPU.
         # NOTE: each Python iteration here is actually made of 2 algorithm iterations
         laplace[self.grid, self.block](self.Ua, self.Ub, self.M, *self.args)
+        vonneumann[self.grid, self.block](self.Ub, self.M, self.Ni, self.Nj, self.Nk, *self.args)
+
         laplace[self.grid, self.block](self.Ub, self.Ua, self.M, *self.args)
+        vonneumann[self.grid, self.block](self.Ua, self.M, self.Ni, self.Nj, self.Nk, *self.args)
 
     def run(self, iterations):
         assert iterations > 0
@@ -305,6 +339,9 @@ class Runner:
         del self.M
         del self.Ua
         del self.Ub
+        del self.Ni_gpu
+        del self.Nj_gpu
+        del self.Nk_gpu
         clear_gpu_memory()
 
 
@@ -314,11 +351,15 @@ def compute_laplacian():
     assert mask.ndim == 3
     assert mask.shape == (N, M, P)
 
+    normal = get_normal(REGION)
+    assert normal.ndim == 4
+    assert normal.shape == (N, M, P, 3)
+
     # Load the current result.
     U = load_npy(filepath(REGION, 'laplacian'))
 
     # GPU Laplacian runner
-    r = Runner(mask, U=U)
+    r = Runner(mask, normal, U=U)
 
     # Run X iterations
     U = r.run(ITERATIONS)
@@ -331,10 +372,10 @@ def compute_laplacian():
 # ------------------------------------------------------------------------------------------------
 
 if __name__ == '__main__':
-    normal = get_normal(REGION)
+    # normal = get_normal(REGION)
 
     # compute_laplacian()
-    # U = load_npy(filepath(REGION, 'laplacian'))
+    U = load_npy(filepath(REGION, 'laplacian'))
 
-    # plt.imshow(normal[500, :, :], interpolation='none', origin='lower')
-    # plt.show()
+    plt.imshow(U[500, :, :], interpolation='none', origin='lower', vmin=-20, vmax=+20)
+    plt.show()
